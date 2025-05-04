152205c8ed140919d598c4cafb1c602d9bc57f71
Aria <ARYAir.121212@gmail.com>
2025-05-04 08:44:57 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let htmlTag = document.getElementById('html')
+
+
+setInterval(()=>{
+        htmlTag.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")"
+        
+}, 2000)
+
+
+
+
+
